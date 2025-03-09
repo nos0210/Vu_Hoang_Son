618b3978cf3b406aa6dcdae61cf624baf80dbf66
Hoang Son <vhsondz2002@gmail.com>
2025-03-09 09:49:32 +0700

    toan.c

--- Vu_Hoang_Son/main.c
+++ Vu_Hoang_Son/main.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
+#include<math.h>
+
 
 void main() {
+	
 	printf("Hello World");
 }
---
+++ Vu_Hoang_Son/toan.c
@@ -0,0 +1,21 @@
+int Cong(int x, int y) {
+	int Tong;
+	Tong = x + y;
+	return Tong;
+}
+
+int Tru(int x, int y) {
+	int Hieu;
+	Hieu = x - y;
+	return Hieu;
+}
+int Nhan(int x, int y) {
+	int Tich;
+	Tich = x * y;
+	return Tich;
+}
+int Chia(int x, int y) {
+	int Thuong;
+		Thuong = x / y;
+		return Thuong;
+}
