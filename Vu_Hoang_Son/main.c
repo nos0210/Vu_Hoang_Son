f631370580c80e9ce6d079d32b53894cbc1e5a9d
Hoang Son <Vhsondz2002@gmail.com>
2025-03-16 09:44:08 +0700

    Add files via upload

--- Vu_Hoang_Son/main.c
+++ Vu_Hoang_Son/main.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
 
 void main() {
-	char str[] = "hello";
+	char str[] = "HeLLo";
 	int cout = 0;
 	int i = 0;
-	while (str[i] != '\0') {
-		i++;
+	while (str[i] != 0 ) {
+	if( str[i] > 65 && str[i] < 92){
 		cout++;
+	}
+		
+	i++;
 	}
 	printf("%d", cout);
 
