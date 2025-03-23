2ef96744ec141e2cac3f723f2569eb9590d012b7
Hoang Son <Vhsondz2002@gmail.com>
2025-03-23 08:36:48 +0700

    Add files via upload

---
+++ Vu_Hoang_Son/bai6.c
@@ -0,0 +1,16 @@
+﻿#include<stdio.h>
+void main() {
+	char str[] = "chu chau chat chuoi";
+	
+	char* prt = str;
+	while (*prt != '0') {
+
+		if (*prt == 'c') {
+			
+			printf("%p ", prt);
+		}
+		*prt++;
+
+	}
+
+}
