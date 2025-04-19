53ee7b477d33fa2d78eddfd6f97ba177d58d85ca
Hoang Son <Vhsondz2002@gmail.com>
2025-04-19 21:25:33 +0700

    Add files via upload

--- Vu_Hoang_Son/main.c
+++ Vu_Hoang_Son/main.c
@@ -1,17 +1,16 @@
 #include<stdio.h>
-#include< ctype.h>
 
 void main() {
-	char str[] = "Xin chao moi nguoi";
-	int i=0;
-	while (str[i] != 0 ) {
-
-	
-		if (str[i] >= 97 && str[i] <= 132){
-			str[i] = str[i] - 32;
-		}
+	char str[] = "hello";
+	int cout = 0;
+	int i = 0;
+	while (str[i] != '\0') {
 		i++;
+		cout++;
 	}
-	printf("%s", str);
-	return 0;
+	printf("%d", cout);
+
+
+
+
 }
